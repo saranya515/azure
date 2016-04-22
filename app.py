f8c269ea64c82577c02462312dd3740d891ecb06
saranya <saranyaks515@gmail.com>
2016-04-22 15:42:10 +0530

    web.py hellowrold push

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-def wsgi_app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    response_body = 'Hello World'
-    yield response_body.encode()
+import web
 
-if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
+urls = ("/.*", "hello")
+app = web.application(urls, globals())
 
-    httpd = make_server('localhost', 5555, wsgi_app)
-    httpd.serve_forever()
+class hello:
+    def GET(self):
+        return 'Hello, world! from web.py'
+
+if __name__ == "__main__":
+    app.run()
