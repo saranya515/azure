6454598c23e4668f57509dca822ef6a28f3a5037
saranya <saranyaks515@gmail.com>
2016-04-22 15:36:31 +0530

    web.py hellowrold

--- rest.py
+++ rest.py
@@ -1,4 +1,11 @@
-import boto3
 import web
 
-print "helloooo"
+urls = ("/.*", "hello")
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self):
+        return 'Hello, world! from web.py'
+
+if __name__ == "__main__":
+    app.run()
